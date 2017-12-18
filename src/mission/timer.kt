6c0ccb874bed329217d29182751fd4bf5571222d
snake <adsloader99@gmail.com>
2017-12-18 16:33:04 +0900

    add new

---
+++ src/mission/timer.kt
@@ -0,0 +1,21 @@
+package mission
+
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
+
+fun main(args : Array<String> ){
+
+    // 타이머생성
+    val timer = Timer("Timer입니다.", true);
+
+    var 카운트 = 0
+
+    // 타이머 기간 정하기
+    timer.scheduleAtFixedRate(1000, 1000) {
+        카운트++
+        println("카운트 -> ${카운트}")
+    }
+
+    // 종료하지 않고 대기하기위함
+    readLine()
+}
