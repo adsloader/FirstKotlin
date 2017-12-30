f794e5fb6c374ff8b9e27bf0b21756a621ebf2fa
adsloader@naver.com <adsloader@naver.com>
2017-12-31 00:19:16 +0900

    commit

---
+++ src/others/property.kt
@@ -0,0 +1,21 @@
+package others
+
+/**
+ * Created by park on 2017-12-31.
+ */
+var 이름 : String = "박모씨"
+    get(){
+        if(field.length > 4) {
+            field =  "외우기 힘듬"
+        }
+        return field
+    }
+    set(s : String ){
+        println (s)
+        field = s
+    }
+
+fun main(args : Array<String> ){
+    이름 = "동작구 에미넴 #2"
+    println (이름)
+}
